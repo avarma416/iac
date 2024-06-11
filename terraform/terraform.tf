4c3de4764a1892122862d67ca634eb7c075886e7
avarma416 <avarma416@yahoo.com>
2024-06-10 19:21:08 -0500

    bn commit

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -9,7 +9,7 @@ terraform {
   backend "s3" {
     bucket = "cloudcoredeepu"
     key    = "terraform.tfstate"
-    region = "us-east-1"
+    region = "us-east-2"
   }
 
   required_version = ">= 1.6.3"
